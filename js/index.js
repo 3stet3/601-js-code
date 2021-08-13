bf1edaac37d8d51084958b388d81f39b39e23a84
3stet3 <little_frog365@ukr.net>
2021-08-13 21:25:03 +0300

    add code

---
+++ js/index.js
@@ -0,0 +1,34 @@
+//var massage = 'Hello js!'; не викор
+//alert(massage);
+//let massage = 'hello js'; можна переприсв
+//const massage = 'hello js'; зміні не можна переприсвоювати
+/*
+let num = 42;
+alert(num);
+num = 4 + 2;
+alert(num);
+const userName = prompt('Enter your name');
+alert('hello '+ userName);
+*/
+const useAge =Number(prompt('enter your age')) ;
+alert('your age ia'+ useAge);
+//const nextAge = useAge + 1 ;
+//alert(nextAge);
+const gender = prompt('enter your gender4');
+alert('you are'+gender);
+// data type
+// Number - Infinity , Nan
+//String - '',"", ``
+//Boolean true , false
+// === дорівнює !== не дорівнюєх і &&
+if (useAge >= 18 && gender === 'male') {
+    alert('you are old');
+}else{
+    alert('you child');
+}
+// зміна add
+
+
+
+
+
